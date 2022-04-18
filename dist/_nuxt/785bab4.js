6c1f01124e5bab51bb773e7d35098243d1fd73c6
cremadev <hello@crema.finance>
2022-04-18 17:59:47 +0800

    update

---
+++ dist/_nuxt/785bab4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[72],{2335:function(n,e,d){"use strict";d.r(e);var l=d(2296).default,t=d(93),component=Object(t.a)(l,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
