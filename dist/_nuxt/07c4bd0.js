cb1255ed0b98c02fcdb33a3e9e86ec673ce440a9
Pisces0818 <anxia0818@gmail.com>
2022-10-17 17:06:31 +0800

    v2 update

---
+++ dist/_nuxt/07c4bd0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[89],{3479:function(n,e,d){"use strict";d.r(e);var l=d(134),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
---
+++ dist/_nuxt/759cccf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[94],{3517:function(n,e,d){"use strict";d.r(e);var l=d(3468).default,t=d(134),component=Object(t.a)(l,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
