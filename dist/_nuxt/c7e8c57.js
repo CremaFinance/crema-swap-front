045897c71946b8886a2aee0c9ffdee305c34467f
cremadevelopment2 <cremadevelopment2@yeah.net>
2024-01-12 19:37:07 +0800

    fix: decimals fixs

--- dist/_nuxt/c7e8c57.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[94],{3869:function(n,e,d){"use strict";d.r(e);var l=d(244),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
--- dist/_nuxt/f14cd86.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[99],{3910:function(n,e,d){"use strict";d.r(e);var l=d(3859).default,t=d(244),component=Object(t.a)(l,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
